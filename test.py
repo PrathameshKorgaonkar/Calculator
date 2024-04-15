d874499c547fd483045f56e9bc70fe2f1316b8af
PrathameshKorgaonkar <prathamesh3032@gmail.com>
2024-04-15 05:42:43 +0000

    updated app.py test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from app import add, subtract, multiply
+from app import add, subtract, multiply, divide
  
 class TestAddition(unittest.TestCase):
     def test_positive_numbers(self):
@@ -22,11 +22,11 @@ class TestSubtraction(unittest.TestCase):
     def test_negative_numbers(self):
         self.assertEqual(subtract(-5, -2), -3)
         self.assertEqual(subtract(-10, -20), 10)
-
+    
     def test_mixed_numbers(self):
         self.assertEqual(subtract(5, -3), 8)
         self.assertEqual(subtract(-10, 5), -15)
-    
+ 
 class TestMultiplication(unittest.TestCase):
     def test_positive_numbers(self):
         self.assertEqual(multiply(2, 3), 6)
@@ -39,6 +39,23 @@ class TestMultiplication(unittest.TestCase):
     def test_mixed_numbers(self):
         self.assertEqual(multiply(2, -3), -6)
         self.assertEqual(multiply(-10, 5), -50)
+ 
+class TestDivision(unittest.TestCase):
+    def test_integer_division(self):
+        self.assertEqual(divide(6, 3), 2)
+        self.assertEqual(divide(10, 2), 20)
+    
+    def test_float_division(self):
+        self.assertEqual(divide(5, 2), 2.5)
+        self.assertAlmostEqual(divide(1, 3), 0.333, places=3)
+    
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(1, 0)
+ 
+    def test_divide_by_negative(self):
+        self.assertEqual(divide(10, -2), 5)
+        self.assertAlmostEqual(divide(8, -4), -2)
  
 if __name__ == '__main__':
     unittest.main()
